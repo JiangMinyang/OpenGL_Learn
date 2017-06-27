b2e3cd8867a19e986f106512a993c56cc568284d
Minyang Jiang <jiangminyangcn@gmail.com>
2017-06-27 17:08:49 -0400

    camera work in progress

--- include/Camera.h
+++ include/Camera.h
@@ -7,18 +7,26 @@
 
 class Camera {
 public:
-  Camera();
-  Camera(const glm::vec3 cameraPosition, const glm::vec3 cameraFront, const glm::vec3 cameraUp);
+  enum MOVEMENT_DIRECTION {
+    FORWARD,
+    BACKWORD,
+    LEFT,
+    RIGHT
+  }
+  static const float DEFAULT_YAW = -90;
+  static const float DEFAULT_PITCH = 0;
+  Camera(const glm::vec3 cameraPosition = glm::vec3(0.0, 0.0, 0.0), const glm::vec3 worldUp = glm::vec3(0.0, 1.0, 0.0), float yaw = DEFAULT_YAW, float pitch = DEFAULT_PITCH);
   glm::mat4 getView();
   Camera& setCameraPosition(const glm::vec3 &cameraPosition);
-  Camera& setCameraFront(const glm::vec3 &cameraFront);
-  Camera& setCameraUp(const glm::vec3 &cameraUp);
 private:
   glm::vec3 cameraPosition;
   glm::vec3 cameraFront;
   glm::vec3 cameraUp;
-  glm::mat4 viewMatrix;
-  void updateView();
+  glm::vec3 cameraRight;
+  glm::vec3 worldUp;
+  float     yaw;
+  float     pitch;
+  void updateVectors();
 };
 
 #endif  //  CAMERA_H
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -2,36 +2,28 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/string_cast.hpp"
 #include <iostream>
-Camera::Camera() : Camera(glm::vec3(0.0, 0.0, 5.0), glm::vec3(0.0, 0.0, -1.0), glm::vec3(0.0, 1.0, 0.0)) {
-  updateView();
-}
 
-Camera::Camera(const glm::vec3 cameraPosition, const glm::vec3 cameraFront, const glm::vec3 cameraUp) 
-  : cameraPosition(cameraPosition), cameraFront(cameraFront), cameraUp(cameraUp) {
-  updateView();
+Camera::Camera(const glm::vec3 cameraPosition, const glm::vec3 worldUp, float yaw, float pitch) {
+  this->cameraPosition = cameraPosition;
+  this->worldUp = worldUp;
+  this->yaw = yaw;
+  this->pitch = pitch;
+  updateVectors();
 }
 
 glm::mat4 Camera::getView() {
-  std::cout << glm::to_string(viewMatrix) << std::endl;
-  return viewMatrix;
-}
-
-void Camera::updateView() {
-  viewMatrix = glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);
+  return glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);
 }
 
 Camera& Camera::setCameraPosition(const glm::vec3 &cameraPosition) {
   this->cameraPosition = cameraPosition;
-  updateView();
   return *this;
 }
-Camera& Camera::setCameraFront(const glm::vec3 &cameraFront) {
-  this->cameraFront = cameraFront;
-  updateView();
-  return *this;
-}
-Camera& Camera::setCameraUp(const glm::vec3 &cameraUp) {
-  this->cameraUp = cameraUp;
-  updateView();
-  return *this;
+
+void Camera::updateVectors() {
+  glm::vec3 front;
+  front.x = cos(glm::radians(pitch)) * cos(glm::radians(pitch));
+  front.y = sin(glm::radians(pitch));
+  front.z = sin(glm::radians(pitch)) * cos(glm::radians(yaw));
+  cameraFront = glm::normalize(front);
 }
